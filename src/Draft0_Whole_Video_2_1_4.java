1a4ada731acc09b7a6c559b3c8978ea0b9c55111
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-06-25 23:50:39 +0300

    Initial commit

---
+++ src/Draft0_Whole_Video_2_1_4.java
@@ -0,0 +1,33 @@
+
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+
+
+////        Пример _ ППППППППППППППППППППППППППППППППППП
+//public class ImageUtils {
+//
+//    static public BufferedImage getImage(String path) throws IOException {
+//        final File file = new File(path);
+//        return ImageIO.read(file);
+//    }
+//    static public void saveImage(BufferedImage, String path) throws IOException {
+//        ImageIO.write(image, "png", new File(path));
+//
+//    }
+//}
+//
+//public class Draft0_Whole_Video_2_1_4 {
+//
+//    public static void main(String[] args) {
+//        System.out.println("Задание : \n1.\t«Обработка фото с помощью лямбд» ч 1\n\nРешение: ");
+//
+//
+//
+//    }
+//}
+//
+////        Конец Примера _ КККККККККККККККК
